f33f03bba325436b6c7c580ea8f7b69c426ae43f
kkong7 <kkong3456@gmail.com>
2021-06-22 21:14:42 +0900

    지도 최초연결

--- src/views/RS_RsLocationsMap.vue
+++ src/views/RS_RsLocationsMap.vue
@@ -1,18 +1,16 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <div id="map_div" />
-      </v-col>
-    </v-row>    
-  </v-container>    
+  <v-row>
+    <v-col style="height:890px;">
+      <div id="map_div" />
+    </v-col>
+  </v-row>       
 </template>
 
 <script>
 
 export default {
   mounted(){
-    window.onload=this.initialize;
+    this.loadScript();
   },
 
   methods:{
@@ -23,6 +21,19 @@ export default {
         mapTypeId:'ROADMAP'
       };
       const map=new olleh.maps.Map(document.getElementById('map_div'),mapOpts);
+    },
+
+    loadScript(){
+      const head=document.getElementsByTagName('head')[0];
+      const script=document.createElement('script');
+      script.type='text/javascript';
+      script.onreadystatechange=function(){
+        if(this.readyState=='complete') this.initialize();
+      }
+
+      script.onload=this.initialize;
+      script.src="https://gis.kt.com/maps/v3.0/sdk.js?key=82a9120746aa758b280c9be03affbaf6fe06f203eee2043da224220bb43d8a3d5516760f";
+      head.appendChild(script);
     }
   }
 
@@ -30,5 +41,6 @@ export default {
 </script>
 
 <style scoped>
-
+#map_div{height:100%};
+html,body{height:100%};
 </style>
